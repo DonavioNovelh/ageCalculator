29ec569b74fa60d3688ea60e5adac6fac2270549
DonavioNovelh <donavioev@gmail.com>
2025-01-03 13:14:09 +0200

    js

---
+++ index.js
@@ -0,0 +1,9 @@
+let inpyear = document.querySelector(".year");
+let inpmonth = document.querySelector(".month");
+let inpday = document.querySelector(".day");
+
+let pyear = document.querySelector(".year");
+let pmonth = document.querySelector(".pmonth");
+let pday = document.querySelector(".pday");
+
+if ()
