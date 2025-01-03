d9c60fa05b8b5d1f603685b1e6b18b49b1c189af
DonavioNovelh <donavioev@gmail.com>
2025-01-03 13:51:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
+let timer = new Date().getFullYear();
+
 let inpyear = document.querySelector(".year");
 let inpmonth = document.querySelector(".month");
 let inpday = document.querySelector(".day");
+
+
+let allinpu = document.querySelectorAll('input')
 
 let pyear = document.querySelector(".year");
 let pmonth = document.querySelector(".pmonth");
 let pday = document.querySelector(".pday");
 
-if ()
+
+//verificar se ta tudo preenchido 
+let emptySite = Array.from(allinpu).every(inp => inp.value.length==0)
+
+function acharano() {
+    if(inpyear.value>=0 && inpyear.value<=timer) {
+        return timer-inpyear.value
+    } 
+}
+
+function acharMes() {
+    if(inpmonth.value<=12) {
+        
+    }
+}
